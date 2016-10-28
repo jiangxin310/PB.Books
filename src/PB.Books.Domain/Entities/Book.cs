5b5ccce75ef937d8599e88ade17d5ce13c27b930
patrick.brick <patrick.brick@PATRICKBRICK>
2016-10-28 08:02:57 -0500

    Add project files.

---
+++ src/PB.Books.Domain/Entities/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PB.Books.Domain.Entities
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Type { get; set; }
+        public DateTime DatePublished { get; set; }
+        public decimal Price { get; set; }
+    }
+}
